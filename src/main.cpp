ff3bf068aa844068a1d37d67236235cc00f3fd73
Sai Ujwal Pasupuleti <ujwal@Sais-MacBook-Pro-2.local>
2026-08-24 01:29:20 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,22 @@ int main() {
   std::string input;
   while (true) {
     std::cout << "$ ";
-    if (!std::getline(std::cin, input)) break;
+    std::getline(std::cin, input);
+    if (input == "exit")
+      break;
     if ( input.starts_with("echo") && input.length() > 4) {
       std::cout<< input.substr(5)<<std::endl;
-      continue;
+    } else if (input.start_with("type")) {
+      std::string s = input.substr(5);
+        if (s == "echo" || s == "exit" || s == "type") {
+          std::cout<< s <<" is a shell builtin"<<std::endl;
+        } else {
+          std::cout<< s <<": not found"<<std::endl;
+        }
     }
-    if (input == "exit") {break;}
-    std::cout<<  input << ": command not found\n";
+    else {
+      std::cout<<  input << ": command not found\n";
+    }
   }
   return 0;
 }
