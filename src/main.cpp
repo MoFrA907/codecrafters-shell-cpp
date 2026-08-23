b303113b50125c78e3189ed8bf00cc16354d761d
Sai Ujwal Pasupuleti <ujwal@Sais-MacBook-Pro-2.local>
2026-08-24 01:16:41 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,15 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   std::string input;
-    while (true) {
-      std::cout << "$ ";
-      std::cin>>input;
-      if ( input.starts_with("echo") ) {
-        std::cout<< input.substr(4);
-        continue;
-      }
-      if (input == "exit") {break;}
-      std::cout<<  input << ": command not found\n";
+  while (true) {
+    std::cout << "$ ";
+    if (!std::getline(std::cin, input)) break;
+    if ( input.starts_with("echo") && input.length() > 4) {
+      std::cout<< input.substr(4);
+      continue;
     }
+    if (input == "exit") {break;}
+    std::cout<<  input << ": command not found\n";
+  }
+  return 0;
 }
