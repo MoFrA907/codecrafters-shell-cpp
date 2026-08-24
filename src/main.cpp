6d61e0258313b00878c2051d4631469d2df88248
Sai Ujwal Pasupuleti <ujwal@Sais-MacBook-Pro-2.local>
2026-08-24 15:10:33 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <sstream>
 #include <string>
-
-int main() {
+#include <cstdlib>
+#include <unistd.h>
+int main(int argc , char * argv[]) {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  // TODO: Uncomment the code below to pass the first stage
+  if (argc != 2 ) { std::cerr << "usage: " << argv[0] << " <cmd>\n"; return 1; }
+  std::string cmd = argv[1];
   std::string input;
   while (true) {
     std::cout << "$ ";
@@ -20,6 +22,19 @@ int main() {
         if (s == "echo" || s == "exit" || s == "type") {
           std::cout<< s <<" is a shell builtin"<<std::endl;
         } else {
+          const char* path_var = std::getenv("PATH");
+          if (!path_var) { std::cout << cmd << ": not found\n"; return 0; }
+          std::istringstream path_stream(path_var);
+          std::string dir;
+          while (std::getline(path_stream ,dir , ':') ){
+            std::string file_path = dir + "/" + cmd;
+            if (access(file_path.c_str() , F_OK) == 0 ) {
+              if (access(file_path.c_str() , X_OK) == 0) {
+                std::cout<< cmd << " is " << file_path << "\n";
+                return 0;
+              }
+            }
+          }
           std::cout<< s <<": not found"<<std::endl;
         }
     }
